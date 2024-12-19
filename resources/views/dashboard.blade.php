d219ad7fd68d5a110201d9c3f0a50f72407cc575
Ingrid Mônica <ingridmonica987@gmail.com>
2024-12-19 16:23:22 -0300

    front de cadastro de evento finalizado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,55 +1,49 @@
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/dashboard.css')}}">
+<link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 @endsection
-
 
 <x-app-layout>
     <div class="container-adicionar-eventos">
+        <h2 class="titulo-secao">Divulgue seu evento conosco</h2>
+        <span class="subtitulo-secao">Envie uma solicitação de postagem de evento para analisarmos</span>
+        
         <div class="container-cadastro-evento">
-            <h2 class="titulo-secao">Divulgue seu evento conosco</h2>
-            <p class="text-comum">Envie uma solicitação de postagem de evento para analisarmos</p>
             <form action="" method="POST" enctype="multipart/form-data">
                 @csrf
-                <h3 class="titulo">Informações do Evento</h4>
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <label for="titulo" class="form-label">Título*</label>
-                        <input type="text" id="titulo" name="titulo" class="form-control" required>
+
+                <h3 class="titulo">Informações do Evento</h3>
+                <div class="form-grid">
+                    <div>
+                        <input type="text" id="titulo" name="titulo" class="input-padrao-evento" placeholder="Título do Evento*" required>
                     </div>
-                    <div class="col-md-6">
-                        <label for="nome_organizador" class="form-label">Nome do Organizador</label>
-                        <input type="text" id="nome_organizador" name="nome_organizador" class="form-control">
+                    <div>
+                        <input type="text" id="nome_organizador" name="nome_organizador" class="input-padrao-evento" placeholder="Nome do Organizador">
                     </div>
-                </div>
-                <div class="mb-3">
-                    <label for="descricao" class="form-label">Descrição</label>
-                    <textarea id="descricao" name="descricao" class="form-control" rows="3"></textarea>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <label for="data_hora_inicio" class="form-label">Data e Hora Início*</label>
-                        <input type="datetime-local" id="data_hora_inicio" name="data_hora_inicio" class="form-control" required>
+                    <div class="full-width">
+                        <textarea id="descricao" name="descricao" class="textarea-padrao" rows="3" placeholder="Descrição do Evento"></textarea>
                     </div>
-                    <div class="col-md-6">
-                        <label for="data_hora_fim" class="form-label">Data e Hora Fim</label>
-                        <input type="datetime-local" id="data_hora_fim" name="data_hora_fim" class="form-control">
+                    <div>
+                        <label for="data_hora_inicio" class="text-comum">Data e Hora Início*</label>
+                        <input type="datetime-local" id="data_hora_inicio" name="data_hora_inicio" class="input-padrao-evento" required>
                     </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <label for="idade_minima" class="form-label">Idade Mínima</label>
-                        <input type="text" id="idade_minima" name="idade_minima" class="form-control">
+                    <div>
+                        <label for="data_hora_fim" class="text-comum">Data e Hora Fim</label>
+                        <input type="datetime-local" id="data_hora_fim" name="data_hora_fim" class="input-padrao-evento">
                     </div>
-                    <div class="col-md-6">
-                        <label for="imagem" class="form-label">Imagem</label>
-                        <input type="file" id="imagem" name="imagem" class="form-control">
+                    <div>
+                        <label for="idade_minima" class="text-comum">Idade mínima</label>
+                        <input type="number" id="idade_minima" name="idade_minima" class="input-padrao-evento" >
+                    </div>
+                    <div>
+                        <label for="imagem" class="text-comum">Imagem do Evento</label>
+                        <input type="file" id="imagem" name="imagem" class="input-padrao-evento imagem">
+                    </div>
+                    <div class="full-width">
+                        <input type="url" id="link" name="link" class="input-padrao-evento" placeholder="Link para mais informações">
                     </div>
                 </div>
-                <div class="mb-3">
-                    <label for="link" class="form-label">Link</label>
-                    <input type="text" id="link" name="link" class="form-control">
-                </div>
-                <div class="mb-3">
+
+                <div class="form-grid mt-2">
                     <div class="form-check">
                         <input type="checkbox" id="fl_ingresso" name="fl_ingresso" class="form-check-input">
                         <label for="fl_ingresso" class="form-check-label">Necessário a compra de ingresso</label>
@@ -59,36 +53,30 @@
                         <label for="fl_gratis" class="form-check-label">Gratuito</label>
                     </div>
                 </div>
-                <h4>Informações do Local</h4>
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <label for="local_nome" class="form-label">Nome do Local*</label>
-                        <input type="text" id="local_nome" name="local_nome" class="form-control" required>
+
+                <h3 class="titulo">Informações do Local</h3>
+                <div class="form-grid">
+                    <div>
+                        <input type="text" id="local_nome" name="local_nome" class="input-padrao-evento" placeholder="Nome do Local*" required>
                     </div>
-                    <div class="col-md-6">
-                        <label for="local_rua" class="form-label">Rua*</label>
-                        <input type="text" id="local_rua" name="local_rua" class="form-control" required>
+                    <div>
+                        <input type="text" id="local_rua" name="local_rua" class="input-padrao-evento" placeholder="Rua*" required>
                     </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-md-4">
-                        <label for="local_cidade" class="form-label">Cidade*</label>
-                        <input type="text" id="local_cidade" name="local_cidade" class="form-control" required>
+                    <div>
+                        <input type="text" id="local_cidade" name="local_cidade" class="input-padrao-evento" placeholder="Cidade*" required>
                     </div>
-                    <div class="col-md-4">
-                        <label for="local_estado" class="form-label">Estado*</label>
-                        <input type="text" id="local_estado" name="local_estado" class="form-control" required>
+                    <div>
+                        <input type="text" id="local_estado" name="local_estado" class="input-padrao-evento" placeholder="Estado*" required>
                     </div>
-                    <div class="col-md-4">
-                        <label for="local_numero" class="form-label">Número*</label>
-                        <input type="text" id="local_numero" name="local_numero" class="form-control" required>
+                    <div class="full-width">
+                        <input type="text" id="local_numero" name="local_numero" class="input-padrao-evento" placeholder="Número*" required>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center">
-                    <button type="submit" class="btn btn-primary">Enviar</button>
+
+                <div class="d-flex justify-content-center mt-4 mb-2">
+                    <button type="submit" class="btn btn-enviar">Enviar</button>
                 </div>
             </form>
-        </div> 
+        </div>
     </div>
-
 </x-app-layout>
